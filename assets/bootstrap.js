2a0a0aaabde9a6989dbb112e67be78d892dcd169
Djobe <jb130675@gmail.com>
2025-04-20 17:56:42 +0200

    ux-turbo

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
-
+import '@hotwired/turbo';
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
